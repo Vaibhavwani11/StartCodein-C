d50d67120588fa62de0168196743f7e8e222edfc
Vaibhavwani11 <66110984+Vaibhavwani11@users.noreply.github.com>
2020-06-29 00:29:33 +0530

    Add files via upload
    Addition of Integers , Double and Float value.

---
+++ addition.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main(){
+	int a;
+	int b;
+	int c;
+        	    printf("Enter the values");
+		scanf("%d\n",&a);
+		scanf("%d",&b);
+	c=a+b;
+	printf("Sum of the numbers=%d\n",c);
+}
---
+++ ans.c
@@ -0,0 +1,9 @@
+//Make addition of two variables and store the result in variable "ans".
+ #include <stdio.h>
+void main(){
+	int a=5;
+	int b=15;
+	int ans;
+	ans=a+b;	
+	printf("%d",ans);
+}
